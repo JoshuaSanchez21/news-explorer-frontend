cf93280b2fb5d26cb67ff1fcfa82f9c450b7e1fd
JoshuaSanchez21 <misael_san12@hotmail.com>
2026-09-15 23:46:05 -0600

    feat: complete desktop main page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,26 +1,51 @@
 import "./Footer.css";
+import facebookIcon from "../../images/facebook.svg";
 
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__copyright">
-        © 2026 NewsExplorer, Powered by News API
-      </p>
+      <p className="footer__copyright">© 2021 Supersite, Powered by News API</p>
 
-      <nav className="footer__navigation">
-        <a className="footer__link" href="/">
-          Inicio
-        </a>
+      <div className="footer__content">
+        <nav className="footer__navigation">
+          <a className="footer__link" href="/">
+            Inicio
+          </a>
 
-        <a
-          className="footer__link"
-          href="https://tripleten.com/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          TripleTen
-        </a>
-      </nav>
+          <a
+            className="footer__link"
+            href="https://practicum.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Practicum
+          </a>
+        </nav>
+
+        <div className="footer__social">
+          <a
+            className="footer__social-link"
+            href="https://github.com/JoshuaSanchez21"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="GitHub"
+          >
+            <svg className="footer__social-icon">
+              <use href="/icons.svg#github-icon" />
+            </svg>
+          </a>
+
+          <a
+            className="footer__social-link"
+            href="https://www.facebook.com/"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="Facebook"
+          >
+            <img className="footer__social-icon" src={facebookIcon} alt="" />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
